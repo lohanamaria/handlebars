13319c2ce0373c31302668475ed8d98e79812694
maria <116275294+lohanamaria@users.noreply.github.com>
2024-06-03 21:31:59 +0000

    slk

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 const app = express();
+app.use(express.unlercoad({extended: true}));
+app.use(express.json());
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
@@ -43,4 +45,13 @@ app.get("/usuario/:id", (req, res) =>{
 
 app.listen(8000, () =>{
     console.log("Servidor executando!");
+});
+
+app.get("/cadastro", (req, res) => {
+    const novoUsuario = {
+        nome: req.body.nome,
+        endereco: req.body.endereco
+    };
+    usuarios.push(novoUsuario);
+    res.redirect("/");
 });
